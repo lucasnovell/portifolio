300088e8f3cbacde3f31023147d5cc16a1f814ec
Lucas Novelli <novellilucas15@gmail.com>
2024-06-26 18:23:56 -0300

    "Alterando o url do fetch"

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/lucasnovell/portifolio-2024/master/data/profile.json?token=GHSAT0AAAAAACTFDFGZQLR52PPVY4MAI4AGZT4QWTA'
+    const url = 'https://raw.githubusercontent.com/lucasnovell/portifolio-2024/master/data/profile.json?token=GHSAT0AAAAAACTFDFGZXKN5C6VMYZ45EOE2ZT4Q4AA'
     const fetching = await fetch(url)
     return await fetching.json()
 } 
